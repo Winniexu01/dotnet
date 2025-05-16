3f7808d1a7c146353b99e90cb5db0af28da4d1a4
Wenwen <v-wexu@microsoft.com>
2025-05-16 15:23:17 +0800

    revert

--- test/Microsoft.DotNet.SourceBuild.Tests/LicenseScanTests.cs
+++ test/Microsoft.DotNet.SourceBuild.Tests/LicenseScanTests.cs
@@ -147,7 +147,7 @@ public class LicenseScanTests : TestBase
         string ignoreOptions = string.Join(" ", s_ignoredFilePatterns.Select(pattern => $"--ignore {pattern}"));
         ExecuteHelper.ExecuteProcessValidateExitCode(
             "scancode",
-            $"--license --processes 4 --max-in-memory 100000000 --timeout {FileScanTimeoutSeconds} --strip-root --only-findings {ignoreOptions} --json-pp {scancodeResultsPath} {Config.LicenseScanPath}",
+            $"--license --processes 4 --timeout {FileScanTimeoutSeconds} --strip-root --only-findings {ignoreOptions} --json-pp {scancodeResultsPath} {Config.LicenseScanPath}",
             OutputHelper);
 
         JsonDocument doc = JsonDocument.Parse(File.ReadAllText(scancodeResultsPath));
